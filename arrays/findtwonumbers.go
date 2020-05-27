23c5406b555f268ec5439bd8175fda634f767ab9
APARNA GURRAM <aparnagk76@gmail.com>
2020-05-26 21:04:21 -0400

    find two numbers

--- arrays/findtwonumbers.go
+++ arrays/findtwonumbers.go
@@ -1,13 +1,13 @@
 package arrays
 
-func findTwoNumbers(a []int, b int) (int, int) {
+func FindTwoNumbers(a []int, b int) (int, int) {
 	for i := 0; i < len(a); i++ {
 		for j := i + 1; j < len(a); j++ {
 			if a[i]+a[j] == b {
 				return a[i], a[j]
 			}
-
 		}
 
 	}
+	return -1, -1
 }
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+
 	arr "github.com/aparnagk76/goexercises/arrays"
 )
 
 func main() {
-	a := []int {"12","45","15","25","46","50","3","102","20"}
-	b := 61
-	x,y := arr.findTwoNumbers(a,b)
-	fmt.Println(x,y)
+	a := []int{12, 45, 15, 25, 46, 50, 3, 102, 20}
+	b := 9
+	x, y := arr.FindTwoNumbers(a, b)
+	fmt.Println(x, y)
 
 }
